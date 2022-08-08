e51326c4c190dc41cdc923407ddac1b5a336a3c7
Shubham <shubhamsatpute432@gmail.com>
2022-08-08 15:50:50 +0530

    this is new commit for nana

---
+++ Code/simple-java-maven-app-master/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,21 @@
+package com.mycompany.app;
+
+/**
+ * Hello Nana loves Akshada!
+
+public class App
+{
+
+    private final String message = "Nana loves Akshada!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(new App().getMessage());
+    }
+
+    private final String getMessage() {
+        return message;
+    }
+
+}
